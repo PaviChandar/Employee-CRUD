25043c2f29c1d2fa11f3db6bc2312e75885fbae4
PaviChandar <pavithrachandar0501@gmail.com>
2023-06-27 19:57:16 +0530

    login-comp

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,19 +1,25 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from 'i18next-http-backend';
 
 i18next
     .use(initReactI18next)
+    .use(Backend)
     .init({
         fallbackLng: "en",
         lng: "en",
         debug: true,
-        resources: {
-            en: {
-                translations: require('./en/translations.json')
-            },
-            ta: {
-                translations: require("./ta/translations.json")
-            }
+        // resources: {
+        //     en: {
+        //         translations: require('./en/translations.json')
+        //     },
+        //     ta: {
+        //         translations: require("./ta/translations.json")
+        //     }
+        // },
+        backend: {
+            loadPath: '/locales/{{en}}/{{translations}}.json',
+            crossDomain: false
         },
         ns: ['translations'],
         keySeparator: false,
