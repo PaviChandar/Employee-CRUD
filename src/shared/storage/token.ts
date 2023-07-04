4c9235cf177f8520c8354b8f5890f848d763bcbe
PaviChandar <pavithrachandar0501@gmail.com>
2023-07-04 11:48:42 +0530

    token

--- src/shared/storage/token.ts
+++ src/shared/storage/token.ts
@@ -1,7 +1,3 @@
-export const token = localStorage.getItem('token')
-export const login = localStorage.getItem('login')
-
 export const localStorageKey = (value: string) => {
-    // localStorage.getItem(JSON.stringify(value));
     return localStorage.getItem(value)
 }
